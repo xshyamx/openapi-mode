dfa898d23860f1534c15180abfb33b494e6a28ba
shyam <xshyamx@gmail.com>
2025-02-22 18:23:36 +0530

    Define separate map for inserts

--- openapi-mode.el
+++ openapi-mode.el
@@ -42,12 +42,12 @@
 (defun is-openapi-file ()
   "Function returns t if current buffer is a openapi file by
 checking if the top-level key 'openapi' is present"
-  (save-excursion
-    (save-restriction
-      (widen)
-      (goto-char (point-min))
+  (save-restriction
+    (widen)
+		(save-excursion
+			(goto-char (point-min))
       (when (re-search-forward "^openapi:" nil t)
-	t))))
+				t))))
 
 (defun openapi--identifier-at-point ()
   "Looks at current line to find if a component reference is present"
@@ -65,88 +65,85 @@ checking if the top-level key 'openapi' is present"
 (defun openapi--list-definitions ()
   "Returns list of all available component definitions"
   (let ((refs) (subsection))
-    (save-excursion
-      (save-restriction
-	(widen)
-	(goto-char (point-min))
-	(when (re-search-forward openapi-components-regexp nil t)
-	  (setq sb (openapi-block-bounds))
-	  (apply #'narrow-to-region sb)
-	  (goto-char (point-min))
-	  (while (re-search-forward openapi-subsection-key-regexp nil t)
-	    (pcase (/ (length (match-string-no-properties 1)) 2)
-	      (1
-	       (setq subsection (match-string-no-properties 2)))
-	      (2
-	       (push (format "components/%s/%s" subsection (match-string-no-properties 2)) refs)))))))
+    (save-restriction
+			(widen)
+			(save-excursion
+				(goto-char (point-min))
+				(when (re-search-forward openapi-components-regexp nil t)
+					(setq sb (openapi-block-bounds))
+					(apply #'narrow-to-region sb)
+					(goto-char (point-min))
+					(while (re-search-forward openapi-subsection-key-regexp nil t)
+						(pcase (/ (length (match-string-no-properties 1)) 2)
+							(1
+							 (setq subsection (match-string-no-properties 2)))
+							(2
+							 (push (format "components/%s/%s" subsection (match-string-no-properties 2)) refs)))))))
     (reverse refs)))
 
 (defun openapi--list-sections ()
   "Returns the lit of sections inside the 'components' key"
-  (save-excursion
-    (save-restriction
-      (widen)
-      (goto-char (point-min))
+  (save-restriction
+    (widen)
+		(save-excursion
+			(goto-char (point-min))
       (when (re-search-forward openapi-components-regexp nil t)
-	(message "found tld")
-	(let ((b (openapi-block-bounds)))
-	  (apply #'narrow-to-region (openapi-block-bounds))
-	  (goto-char (point-min))
-	  (let (sections)
-	    (while (re-search-forward (format "^%s\\([[:word:]]+\\):" (make-string 2 ? )) nil t)
-	      (push
-	       (cons (format "components/%s" (match-string-no-properties 1)) (match-beginning 1))
-	       sections))
-	    (reverse sections)))))))
+				(message "found tld")
+				(let ((b (openapi-block-bounds)))
+					(apply #'narrow-to-region (openapi-block-bounds))
+					(goto-char (point-min))
+					(let (sections)
+						(while (re-search-forward (format "^%s\\([[:word:]]+\\):" (make-string 2 ? )) nil t)
+							(push
+							 (cons (format "components/%s" (match-string-no-properties 1)) (match-beginning 1))
+							 sections))
+						(reverse sections)))))))
 
 (defun openapi--find-references (symbol)
   "Find schema references within the buffer of the
 form $ref: '#/components/subsection/key' for the SYMBOL"
-  (save-excursion
-    (save-restriction
-      (widen)
-      (goto-char (point-min))
+  (save-restriction
+    (widen)
+		(save-excursion
+			(goto-char (point-min))
       (let (refs)
-	(while (re-search-forward (format "#/%s" symbol) nil t)
-	  (push
-	   (xref-make
-	    (format
-	     "%s:%s"
-	     (propertize (format "%s" (line-number-at-pos (line-beginning-position))) 'face 'shadow)
-	     (buffer-substring (line-beginning-position) (line-end-position))
-	     )
-	    (xref-make-buffer-location (current-buffer) (match-beginning 0)))
-	   refs))
-	(append (reverse refs) (openapi--find-definitions symbol))))))
+				(while (re-search-forward (format "#/%s" symbol) nil t)
+					(push
+					 (xref-make
+						(format
+						 "%s:%s"
+						 (propertize (format "%s" (line-number-at-pos (line-beginning-position))) 'face 'shadow)
+						 (buffer-substring (line-beginning-position) (line-end-position))
+						 )
+						(xref-make-buffer-location (current-buffer) (match-beginning 0)))
+					 refs))
+				(append (reverse refs) (openapi--find-definitions symbol))))))
 
 (defun openapi--find-definitions (symbol)
   "Return the ref objects for the definitions found for SYMBOL
 within the buffer"
   (let* ((tokens (split-string symbol "/"))
-	 (section (car tokens))
-	 (subsection (cadr tokens))
-	 (key (caddr tokens)))
+				 (section (car tokens))
+				 (subsection (cadr tokens))
+				 (key (caddr tokens)))
     (when (and section subsection key)
-      (save-excursion
-	(save-restriction
-	  (widen)
-	  (goto-char (point-min))
-	  (when (re-search-forward (format "^%s:" section) nil t)
-	    (setq sb (openapi-block-bounds))
-	    (apply #'narrow-to-region sb)
-	    (goto-char (point-min))
-	    ;; (message "looking at section : '%s'" (buffer-substring-no-properties (point-min) (point-max)))
-	    (when (re-search-forward (format "^%s%s:" (make-string 2 ? ) subsection) (cadr sb) t)
-	      (setq ssb (openapi-block-bounds))
-	      (apply #'narrow-to-region ssb)
-	      (goto-char (point-min))
-	      ;; (message "looking at subsection: '%s'" (buffer-substring-no-properties (point-min) (point-max)))
-	      (when (re-search-forward (format "^%s%s:" (make-string 4 ? ) key) nil t)
-		;; (message "match : '%s' %d / %" (match-string-no-properties 0) (match-beginning 0) (line-beginning-position))
-		(list
-		 (xref-make
-		  (buffer-substring-no-properties (line-beginning-position) (line-end-position))
-		  (xref-make-buffer-location (current-buffer) (match-beginning 0))))))))))))
+      (save-restriction
+				(widen)
+				(save-excursion
+					(goto-char (point-min))
+					(when (re-search-forward (format "^%s:" section) nil t)
+						(setq sb (openapi-block-bounds))
+						(apply #'narrow-to-region sb)
+						(goto-char (point-min))
+						(when (re-search-forward (format "^%s%s:" (make-string 2 ? ) subsection) (cadr sb) t)
+							(setq ssb (openapi-block-bounds))
+							(apply #'narrow-to-region ssb)
+							(goto-char (point-min))
+							(when (re-search-forward (format "^%s%s:" (make-string 4 ? ) key) nil t)
+								(list
+								 (xref-make
+									(buffer-substring-no-properties (line-beginning-position) (line-end-position))
+									(xref-make-buffer-location (current-buffer) (match-beginning 0))))))))))))
 
 (defun openapi-jump-to-section ()
   "Jumps to selected component section"
@@ -206,20 +203,30 @@ within the buffer"
      ,(concat "Insert " section)
      (interactive)
      (when-let
-	 ((selection
-	   (completing-read
-	    ,(format "Select %s: " (openapi--section-singular section)) (openapi--list-section ,section)
-	    nil t ,(format "components/%s/" section))))
+				 ((selection
+					 (completing-read
+						,(format "Select %s: " (openapi--section-singular section)) (openapi--list-section ,section)
+						nil t ,(format "components/%s/" section))))
        (insert
-	(format
-	 (if (looking-back openapi-yaml-ref-regexp (line-beginning-position))
-	     (if (> (length (match-string-no-properties 2)) 0)
-		 "#/%s"
-	       "'#/%s'"
-	       )
-	   "$ref: '#/%s'"
-	   )
-	 selection)))))
+				(format
+				 (if (looking-back openapi-yaml-ref-regexp (line-beginning-position))
+						 (if (> (length (match-string-no-properties 2)) 0)
+								 "#/%s"
+							 "'#/%s'"
+							 )
+					 "$ref: '#/%s'"
+					 )
+				 selection)))))
+
+(defvar openapi--insert-map
+	(let ((keymap (make-sparse-keymap)))
+		(define-key keymap (kbd "p") (openapi--insert-section "parameters"))
+    (define-key keymap (kbd "h") (openapi--insert-section "headers"))
+    (define-key keymap (kbd "s") (openapi--insert-section "schemas"))
+    (define-key keymap (kbd "r") (openapi--insert-section "responses"))
+    (define-key keymap (kbd "b") (openapi--insert-section "requestBodies"))
+		keymap)
+	"OpenAPI keymap for inserting sections")
 
 (defvar openapi-mode-map
   (let ((keymap (make-sparse-keymap)))
@@ -227,14 +234,9 @@ within the buffer"
     (define-key keymap (kbd "C-c C-p") #'openapi-jump-to-operation)
     (define-key keymap (kbd "C-c C-s") #'openapi-jump-to-section)
     (define-key keymap (kbd "C-c C-c") #'openapi-insert-component-ref)
-    (define-key keymap (kbd "C-c i p") (openapi--insert-section "parameters"))
-    (define-key keymap (kbd "C-c i h") (openapi--insert-section "headers"))
-    (define-key keymap (kbd "C-c i s") (openapi--insert-section "schemas"))
-    (define-key keymap (kbd "C-c i r") (openapi--insert-section "responses"))
-    (define-key keymap (kbd "C-c i b") (openapi--insert-section "requestBodies"))
+		(define-key keymap (kbd "C-c i") openapi--insert-map)
     keymap)
-  "OpenAPI keymap for block & path-operation navigation"
-  )
+  "OpenAPI keymap for block & path-operation navigation")
 
 (define-minor-mode openapi-mode
   "Minor mode to provide xref navigation within OpenAPI yaml files"
